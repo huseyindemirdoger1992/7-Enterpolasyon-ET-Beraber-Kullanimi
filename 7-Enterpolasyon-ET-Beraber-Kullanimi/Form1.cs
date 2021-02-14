57e466254b2b10823277ea27edb11fa3fecd8b21
Hüseyin DEMİRDÖĞER <huseyindemirdoger1992@hotmail.com>
2021-02-14 07:18:45 +0300

    Add project files.

---
+++ 7-Enterpolasyon-ET-Beraber-Kullanimi/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _7_Enterpolasyon_ET_Beraber_Kullanimi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int s1, s2;
+            s1 = 8;
+            s2 = 4;
+            int Sonuc = 8 / 4;
+            // Dolar işareti ile skop içlerine değer ataması yapılabilirken 
+            // @ işareti ile de sağ ve sol slaş lar kullanılabilinir.
+            string Ornek = $@"{s1} \ {s2} Sonucu  {Sonuc}";
+
+        }
+    }
+}
